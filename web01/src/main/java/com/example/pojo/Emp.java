31395a5c69086b0d3247b983fd586519f2b728cf
renyanyu <3470163138@qq.com>
2026-03-29 17:53:33 +0800

    emp,empexpr

---
+++ web01/src/main/java/com/example/pojo/Emp.java
@@ -0,0 +1,20 @@
+package com.example.pojo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Emp {
+   private Integer id;
+   private String username;
+   private String password;
+   private String name;
+   private Short gender;
+   private String Phone;
+   private Integer job;
+   private Integer salary;
+   private String image;
+   private LocalDate entryDate;
+   private Integer deptId;
+   private LocalDateTime createDate;
+   private LocalDateTime updateDate;
+}
---
+++ web01/src/main/java/com/example/pojo/EmpExpr.java
@@ -0,0 +1,13 @@
+package com.example.pojo;
+
+import java.time.LocalDate;
+
+public class EmpExpr {
+    private Integer id;
+    private Integer empId;
+    private LocalDate begin;
+    private LocalDate end;
+    private String company;
+    private String job;
+
+}
