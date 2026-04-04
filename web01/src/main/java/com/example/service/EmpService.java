0c2cc6598475c0268d0f37252dbf3bc2586081a2
renyanyu <3470163138@qq.com>
2026-04-04 19:41:45 +0800

    change

--- web01/src/main/java/com/example/service/EmpService.java
+++ web01/src/main/java/com/example/service/EmpService.java
@@ -9,4 +9,6 @@ import java.time.LocalDate;
 public interface EmpService {
     // PageResult<Emp> page(Integer page, Integer pageSize, String name, Integer gender, LocalDate begin, LocalDate end);
     PageResult<Emp> page(EmpQueryParam empQueryParam);
+
+    void save(Emp emp);
 }
--- web01/src/main/java/com/example/service/impl/EmpServiceImpl.java
+++ web01/src/main/java/com/example/service/impl/EmpServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -45,4 +46,13 @@ public PageResult<Emp> page(EmpQueryParam empQueryParam)
     Page<Emp> p = (Page<Emp>) rows;
     return new PageResult<Emp>(p.getTotal(),p.getResult());
 }
+
+    @Override
+    public void save(Emp emp) {
+        emp.setCreateDate(LocalDateTime.now());
+        emp.setUpdateDate(LocalDateTime.now());
+
+        empMapper.insert(emp);
+    }
+
 }
